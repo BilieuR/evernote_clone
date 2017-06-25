508096f75a53cc6511a37f7b16e02b70f40d9405
BilieuR <bilieureath1@gmail>
2017-06-25 10:49:39 +1000

    Change root of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
-
-  get 'welcome/index'
 
   resources :docs
+  authenticated :user do
+    root 'docs#index', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
+  get 'welcome/index'
 end
